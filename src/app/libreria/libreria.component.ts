3a2efd29bb09b1f385443d308ce0d3cf30cf02f1
Alejandro <alejandro.pons98@gmail.com>
2020-08-22 18:20:58 +0200

    Añadido filtrar libros según si están activos o no

--- src/app/libreria/libreria.component.ts
+++ src/app/libreria/libreria.component.ts
@@ -7,12 +7,14 @@ import {Libro} from '../interfaces/libro';
   styleUrls: ['./libreria.component.scss']
 })
 export class LibreriaComponent implements OnInit {
+  copiaLibros: Libro[];
   libros: Libro[];
+  haSeleccionadoTipoLibro: boolean;
 
   constructor() { }
 
   ngOnInit(): void {
-    this.libros = [
+    this.copiaLibros = [
       {
         cod: '1',
         titulo: 'libro 1',
@@ -30,5 +32,44 @@ export class LibreriaComponent implements OnInit {
         activo: false
       }
     ];
+
+    this.libros = [];
+    this.haSeleccionadoTipoLibro = false;
+  }
+
+  mostrarLibrosActivos() {
+    this.haSeleccionadoTipoLibro = true;
+    this.libros = this.filtrarLibros('activos');
+  }
+
+  mostrarLibrosNoActivos() {
+    this.haSeleccionadoTipoLibro = true;
+    this.libros = this.filtrarLibros('');
+  }
+
+  filtrarLibros(tipoFiltro: string): Libro[] {
+    let librosFiltrados: Libro[] = [];
+
+    if (tipoFiltro === 'activos') {
+      this.copiaLibros.forEach(libro => {
+        if (libro.activo) {
+          librosFiltrados.push(libro);
+        }
+      });
+    }
+    else {
+      this.copiaLibros.forEach(libro => {
+        if (!libro.activo) {
+          librosFiltrados.push(libro);
+        }
+      });
+    }
+    
+    return librosFiltrados;
+  }
+
+  mostrarTodosLibros() {
+    this.haSeleccionadoTipoLibro = true;
+    this.libros = this.copiaLibros;
   }
 }
