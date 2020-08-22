c80ef90c30445de9bdb8cf16df5e50bc607eb1ec
Alejandro <alejandro.pons98@gmail.com>
2020-08-22 17:11:56 +0200

    Creada interfaz Libro y añadido varios libros

---
+++ src/app/interfaces/libro.ts
@@ -0,0 +1,8 @@
+export interface Libro {
+    cod: string,
+    titulo: string,
+    precio: number,
+    imagen: string,
+    autor: string,
+    activo: boolean
+}
--- src/app/libreria/libreria.component.ts
+++ src/app/libreria/libreria.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Libro} from '../interfaces/libro';
 
 @Component({
   selector: 'app-libreria',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./libreria.component.scss']
 })
 export class LibreriaComponent implements OnInit {
+  libros: Libro[];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.libros = [
+      {
+        cod: '1',
+        titulo: 'libro 1',
+        precio: 45,
+        imagen: null,
+        autor: 'Perez Reverte',
+        activo: true
+      },
+      {
+        cod: '2',
+        titulo: 'libro 2',
+        precio: 50,
+        imagen: null,
+        autor: 'Miguel de Cervantes',
+        activo: false
+      }
+    ];
   }
-
 }
