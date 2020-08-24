302d8fc3ab0b3555c9d8872a152a87ae67c7b91b
alejandro <alejandro.pons98@gmail.com>
2020-08-24 19:47:59 +0200

    Eliminado métodos que filtraban array de libros mediante javascript

--- src/app/libreria/libreria.component.ts
+++ src/app/libreria/libreria.component.ts
@@ -8,7 +8,6 @@ import {LibrosService} from '../servicios/libros.service';
   styleUrls: ['./libreria.component.scss']
 })
 export class LibreriaComponent implements OnInit {
-  copiaLibros: Libro[];
   libros: Libro[];
   textoBusqueda: string;
   nombreCampoOrdenar: string;
@@ -17,13 +16,10 @@ export class LibreriaComponent implements OnInit {
 
   ngOnInit(): void {
     this.librosService.obtenerLibros().subscribe(libros => {
-      this.copiaLibros = libros
+      this.libros = libros;
 
       this.textoBusqueda = null; 
       this.nombreCampoOrdenar = 'ninguno';
-
-      this.mostrarTodosLibros();    // Mostramos todos los libros para que podamos seleccionar si queremos filtrar por si están activos o no, o por
-                                    // título o autor.  
     },
     error => alert('Ha habido un error al obtener los libros: ' + error.error.mensaje));      
   }
@@ -42,29 +38,11 @@ export class LibreriaComponent implements OnInit {
     error => alert('Ha habido un error al obtener los libros no activos: ' + error.error.mensaje));
   }
 
-  filtrarLibros(tipoFiltro: string): Libro[] {
-    let librosFiltrados: Libro[] = [];
-
-    if (tipoFiltro === 'activos') {
-      this.copiaLibros.forEach(libro => {
-        if (libro.activo) {
-          librosFiltrados.push(libro);
-        }
-      });
-    }
-    else {
-      this.copiaLibros.forEach(libro => {
-        if (!libro.activo) {
-          librosFiltrados.push(libro);
-        }
-      });
-    }
-    
-    return librosFiltrados;
-  }
-
   mostrarTodosLibros() {
-    this.libros = this.copiaLibros;
+    this.librosService.obtenerLibros().subscribe(libros => {
+      this.libros = libros;
+    },
+    error => alert('Ha habido un error al obtener los libros: ' + error.error.mensaje));
   }
 
   cambiarOrdenacionLibros() {
